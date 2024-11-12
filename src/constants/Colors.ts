3701d236502b4f789d64d51712a4d8994355500c
Noel Paul <noelpaul@rapidera.com>
2024-11-12 20:57:52 +0530

    Adding Constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,15 @@
+export const Colors = {
+  appBg: '#F6F7FC',
+  primaryGreen: '#43b05c',
+  lightGreen: '#53D671',
+  greenYellow: '#B0FE76',
+  ebony: '#595A4A',
+  jet: '#363635',
+  lightBlack: '#1B1B1A',
+  black: '#000000',
+  white: '#fff',
+  orange: '#CE3F20',
+  bgGray: '#FFFFFF4D',
+  fontGray: '#858585',
+  transparentWhite90: '#FFFFFFE6',
+};
